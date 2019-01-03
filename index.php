5c286424d9e80ff0f80c8e3cc048edf0454b55a2
khaki-ranger <yamanogomora@gmail.com>
2019-01-04 05:02:05 +0900

    #08 bindParam()を使ってみよう

--- index.php
+++ index.php
@@ -15,11 +15,13 @@ try {
   
   $name = 'taguchi';
   $stmt->bindValue(1, $name, PDO::PARAM_STR);
-  $score = 23;
-  $stmt->bindValue(2, $score, PDO::PARAM_INT);
+
+  $stmt->bindParam(2, $score, PDO::PARAM_INT);
+  $score = 52;
   $stmt->execute();
   $score = 44;
-  $stmt->bindValue(2, $score, PDO::PARAM_INT);
+  $stmt->execute();
+  $score = 6;
   $stmt->execute();
 
   echo "inserted: " . $db->lastInsertId();

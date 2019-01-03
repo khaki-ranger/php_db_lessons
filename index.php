62f383d3a99475a26809ec040fae6413ef74a23f
khaki-ranger <yamanogomora@gmail.com>
2019-01-04 05:30:14 +0900

    #11 PDO::FETCH_CLASSを使ってみよう

--- index.php
+++ index.php
@@ -5,25 +5,27 @@ define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORd', 'kiheitai79');
 define('PDO_DSN', 'mysql:host=localhost;dbname=' . DB_DATABASE);
 
+class User {
+  // public $id;
+  // public $name;
+  // public $score;
+
+  public function show() {
+    echo "$this->name ($this->score)";
+  }
+}
+
 try {
   // connect
   $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORd);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // select
-  // $stmt = $db->prepare("select score from users where score > ?");
-  // $stmt->execute([60]);
-
-  // $stmt = $db->prepare("select name from users where name like ?");
-  // $stmt->execute(['%t%']);
-
-  $stmt = $db->prepare("select score from users order by score desc limit ?");
-  $stmt->bindvalue(1, 1, PDO::PARAM_INT);
-  $stmt->execute();
-
-  $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // FETCH_CLASS
+  $stmt = $db->query("select * from users");
+  $users = $stmt->fetchAll(PDO::FETCH_CLASS, 'User');
   foreach ($users as $user) {
-    var_dump($user);
+    $user->show();
   }
   echo $stmt->rowCount() . " records found,";
 
